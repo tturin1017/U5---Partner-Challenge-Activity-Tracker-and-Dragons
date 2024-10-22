ab8d50a03b271552e63cb7c5de52659e738eb12b
Tatiana Turin <146445767+tturin1017@users.noreply.github.com>
2024-10-22 16:36:26 +0000

    final

--- src/main/java/com/example/project/ActivityTracker.java
+++ src/main/java/com/example/project/ActivityTracker.java
@@ -1,51 +1,8 @@
 package com.example.project;
-
+//Since the class it completely empty. You will run into errors because the test cases are looking for your methods. You can still build and test 
+//in your main method. Make sure when you run your program you go back to terminal view. You will see your output. 
 public class ActivityTracker {
-    private int targetGoal;
-    private int totalMinutes;
-    private int sessions;
-    private int maxMinutes;
-
-    public ActivityTracker(int targetGoal) {
-        this.targetGoal = targetGoal;
-        totalMinutes = 0; // default value
-        sessions = 0; // default value
-        maxMinutes = 0; // default value
-    }
-
-    public int getTargetGoal(){
-        return targetGoal;
-    }
-
-    public int getTotalMinutes() {
-        return totalMinutes;
-    }
-
-    public int getSessions() {
-        return sessions;
-    }
-
-    public int getMaxMinutes() {
-        return maxMinutes;
-    }
-
-    public void logMinutes(int minutes) {
-        totalMinutes += minutes;
-        sessions++;
-        if (minutes > maxMinutes) {
-            maxMinutes = minutes;
-        }
-    }
-
-    public boolean goalAchieved() {
-        return (totalMinutes >= targetGoal);
-
-        // or:  if (totalMinutes >= targetGoal) {
-        //         return true;
-        //      } else {
-        //         return false;
-        //      }
-    }
+    
 }
 
 
--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,30 +1,9 @@
 package com.example.project;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.Scanner;
+
 
 public class Main {
 
    public static void main(String[] args) {
-     Dragon dragon = new Dragon();
-        dragon.attack();
-        dragon.takeDamage(20);
-        dragon.powerUp(1);
-        dragon.powerUp(2);
-        dragon.powerUp(2);
-        dragon.powerUp(8);
-        dragon.state();
-        dragon.attack();
-        
-
-        PrintStream originalOut = System.out;
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        PrintStream ps = new PrintStream(bos);
-        System.setOut(ps);
-        dragon.takeDamage(70);
-        ps.flush();
-        System.setOut(originalOut);
-        System.out.println("---"+bos.toString());
-        
+     System.out.println("hello");
    }
 }
